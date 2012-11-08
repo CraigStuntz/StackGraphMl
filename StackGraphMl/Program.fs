63edc3f0ed3605eeba0be4d3978481497555d8f8
Craig Stuntz <craig_stuntz@acm.org>
2012-11-08 11:12:26 -0500

    Now reading users as nodes.

--- StackGraphMl/Program.fs
+++ StackGraphMl/Program.fs
@@ -8,24 +8,29 @@ open System.Xml.Schema
 open Microsoft.FSharp.Data.TypeProviders
 open Microsoft.FSharp.Linq
 
-let minQuestionVotes = 100 // Smaller min = more questions in result = slower
 let folder = @"E:\StackExchangeDump";
-
-type Answer = { Id: int; UserId: int; Score: int; ParentId: int }
-type Question = { Id: int; UserId: int; Score: int; Answers: Answer list }
-type User = { UserId: int }
 
 let parseInt = System.Int32.TryParse >> function
     | true, v -> v
     | false, _ -> 0
  
+type Answer = { Id: int; UserId: int; Score: int; ParentId: int }
+type Question = { Id: int; UserId: int; Score: int; Answers: Answer list }
+type User = { UserId: int; Reputation: int }
+
 let readQuestion (reader: XmlReader) = 
     { Id = parseInt(reader.["Id"]); UserId = parseInt(reader.["UserId"]); Score = parseInt(reader.["Score"]); Answers = List.empty }
 
 let readAnswer (reader: XmlReader) : Answer = 
     { Id = parseInt(reader.["Id"]); UserId = parseInt(reader.["UserId"]); Score = parseInt(reader.["Score"]); ParentId = parseInt(reader.["ParentId"]) }
 
-let readRow (reader: XmlReader) (qs: Question list, anss: Answer list) = 
+let readUser (reader: XmlReader) : User option = 
+    if reader.Read() then
+        Some ( { UserId = parseInt(reader.["Id"]); Reputation = parseInt(reader.["Reputation"]) } )
+    else
+        None
+
+let readPostRow (reader: XmlReader) (qs: Question list, anss: Answer list) = 
     if reader.Read() then
         match reader.["PostTypeId"] with
             | "1" -> ( readQuestion reader :: qs, anss)
@@ -44,49 +49,89 @@ let logIteration (seq, itemName) =
     seq |> Seq.mapi(fun i x -> log(i + 1, itemName); x)
 
 let readAllRows (reader : XmlReader) = 
-    seq { 
-        // while reader.IsStartElement("row") do
-        for i in 1..100 do 
-            reader.ReadStartElement("row")
-            yield reader 
-    }
+    // Files are huge; let's take just a few sample records for debugging
+    if System.Diagnostics.Debugger.IsAttached then    
+        seq { 
+            for i in 1..100 do 
+                reader.ReadStartElement("row")
+                yield reader
+        }
+    else 
+        seq {
+            while reader.IsStartElement("row") do
+                reader.ReadStartElement("row")
+                yield reader
+        }
 
-let writeGraphML (users: User seq) = 
-    use outFile = new FileStream(Path.Combine(folder, "StackOverflow.gml"), FileMode.Create, FileAccess.Write)
-    let settings = new XmlWriterSettings()
-    settings.Indent <- true 
-    use writer = XmlWriter.Create(outFile, settings)
-    writer.WriteStartElement("graphml", "http://graphml.graphdrawing.org/xmlns")
-    writer.WriteAttributeString("xmlns", "xsi", null, XmlSchema.InstanceNamespace)
-    writer.WriteAttributeString("xsi", "schemaLocation", null, "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
-    writer.WriteStartElement("graph")
-    writer.WriteAttributeString("id", "G")
-    writer.WriteAttributeString("edgedefault", "directed")
-    users |> Seq.iter(fun u -> writer.WriteStartElement("node"); writer.WriteAttributeString("id", u.UserId.ToString()); writer.WriteEndElement())
-    writer.WriteEndElement()
-    writer.WriteEndElement()
-    writer.Flush()
-    writer.Close()
+let writeUser (writer: XmlWriter) user =
+    match user with 
+        | Some u -> 
+            writer.WriteStartElement("node")
+            writer.WriteAttributeString("id", u.UserId.ToString())
+            if u.Reputation <> 0 then // 0 is default value; saves space in file to not write is out since huge number of users with rep = 0
+                writer.WriteStartElement("data")
+                writer.WriteAttributeString("key", "rep")
+                writer.WriteValue(u.Reputation)
+                writer.WriteEndElement()
+            writer.WriteEndElement()
+        | None -> printfn "Bad user record found. Ignoring."
 
-let importStackData () = 
+let importUsers(writer: User option -> unit) = 
+    use inFile = new FileStream(Path.Combine(folder, "users.xml"), FileMode.Open, FileAccess.Read)
+    use reader = XmlReader.Create(inFile)
+    reader.MoveToElement() |> ignore
+    reader.ReadStartElement("users")
+    logIteration(readAllRows reader, "users") |> Seq.iter(fun reader -> readUser(reader) |> writer)
+
+let importPosts () = 
     use inFile = new FileStream(Path.Combine(folder, "posts.xml"), FileMode.Open, FileAccess.Read)
     use reader = XmlReader.Create(inFile)
     reader.MoveToElement() |> ignore
     reader.ReadStartElement("posts")
     let z: Question list * Answer list = (List.empty, List.Empty)
-    let (qs, anss) = logIteration(readAllRows(reader), "posts") |> Seq.fold(fun accum elem -> readRow elem accum) z 
+    let (qs, anss) = logIteration(readAllRows reader, "posts") |> Seq.fold(fun accum elem -> readPostRow elem accum) z 
     let la = List.length(anss)
-    let lq = List.length(anss)
+    let lq = List.length(qs)
     printfn "Read %d questions and %d answers" lq la
-    0
+
+let writeGraphML () = 
+    use outFile = new FileStream(Path.Combine(folder, "StackOverflow.gml"), FileMode.Create, FileAccess.Write)
+    let settings = new XmlWriterSettings()
+    if System.Diagnostics.Debugger.IsAttached then
+        settings.Indent <- true 
+    use writer = XmlWriter.Create(outFile, settings)
+    // Necessary GraphML headers
+    writer.WriteStartElement("graphml", "http://graphml.graphdrawing.org/xmlns")
+    writer.WriteAttributeString("xmlns", "xsi", null, XmlSchema.InstanceNamespace)
+    writer.WriteAttributeString("xsi", "schemaLocation", null, "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
+    // Custom GraphML-Attributes
+    writer.WriteStartElement("key")
+    writer.WriteAttributeString("id", "rep")
+    writer.WriteAttributeString("for", "node")
+    writer.WriteAttributeString("attr.name", "reputation")
+    writer.WriteAttributeString("attr.type", "int")
+    writer.WriteElementString("default", "0")
+    writer.WriteEndElement()
+    // Graph element
+    writer.WriteStartElement("graph")
+    writer.WriteAttributeString("id", "G")
+    writer.WriteAttributeString("edgedefault", "directed")
+    // Nodes
+    printfn "Reading users.xml"
+    importUsers(writeUser(writer))
+    // Edges
+    printfn "Reading posts.xml"
+    importPosts()
+    // Finish document
+    writer.WriteEndElement()
+    writer.WriteEndElement()
+    writer.Flush()
+    writer.Close()
 
 [<EntryPoint>]
 let main argv = 
     printfn "Started import at %A." System.DateTime.Now
-    printfn "Reading posts.xml"
-    importStackData()  |> ignore
-    printfn "Writing GraphML"
-    writeGraphML(seq { for uid in 1..10 -> { UserId = uid } })
+    writeGraphML()
     printfn "Finished import at %A." System.DateTime.Now
     Console.ReadLine() |> ignore
     0
